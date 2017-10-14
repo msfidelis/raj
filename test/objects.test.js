9032d7dc884428ae48db9d45e2c16b19e6a7a021
eptaccio <eptaccio@gmail.com>
2017-10-13 22:46:11 -0300

    adicionado metodo de objeto

---
+++ test/objects.test.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const raj = require("../lib/raj");
+
+const expect = require("chai").expect;
+
+describe("#Objects Test", () => {
+
+  it("#raj.selectFromObject", () => {
+
+    const object = {
+      name: 'hello',
+      id: 3123423,
+      lorem: 'ipsum',
+      music: 'hello darkness my old friend',
+      raj: 'http://awesome'
+    }
+
+    expect(raj.selectFromObject(object, ['name', 'id'])).to.deep.equal({name: 'hello', id: 3123423})
+  });
+
+});
